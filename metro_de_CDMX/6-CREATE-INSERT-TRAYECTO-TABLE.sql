f1518400e9d30465e314b586adbdd6af36facd96
Argenis Chirinos <argenisachc@gmail.com>
2023-04-29 00:27:47 -0400

    insertando informacion en la tabla trayecto

--- metro_de_CDMX/6-CREATE-INSERT-TRAYECTO-TABLE.sql
+++ metro_de_CDMX/6-CREATE-INSERT-TRAYECTO-TABLE.sql
@@ -26,3 +26,54 @@ CREATE TABLE IF NOT EXISTS public.trayecto
 -- Establecer el propietario de la tabla en postgres
 ALTER TABLE public.trayecto
     OWNER TO postgres;
+
+insert into trayecto (id_trains, id_stations) values (30, 'rojo');
+insert into trayecto (id_trains, id_stations) values (47, 'oro');
+insert into trayecto (id_trains, id_stations) values (29, 'morado');
+insert into trayecto (id_trains, id_stations) values (45, 'rojo');
+insert into trayecto (id_trains, id_stations) values (13, 'oro');
+insert into trayecto (id_trains, id_stations) values (37, 'rosa');
+insert into trayecto (id_trains, id_stations) values (9, 'naranja');
+insert into trayecto (id_trains, id_stations) values (38, 'rosa');
+insert into trayecto (id_trains, id_stations) values (20, 'rojo');
+insert into trayecto (id_trains, id_stations) values (28, 'rosa');
+insert into trayecto (id_trains, id_stations) values (14, 'morado');
+insert into trayecto (id_trains, id_stations) values (5, 'cian');
+insert into trayecto (id_trains, id_stations) values (1, 'morado');
+insert into trayecto (id_trains, id_stations) values (11, 'café');
+insert into trayecto (id_trains, id_stations) values (2, 'oro');
+insert into trayecto (id_trains, id_stations) values (25, 'cian');
+insert into trayecto (id_trains, id_stations) values (30, 'rosa');
+insert into trayecto (id_trains, id_stations) values (38, 'verde_olivo');
+insert into trayecto (id_trains, id_stations) values (19, 'verde_y_gris');
+insert into trayecto (id_trains, id_stations) values (22, 'morado');
+insert into trayecto (id_trains, id_stations) values (44, 'café');
+insert into trayecto (id_trains, id_stations) values (25, 'amarillo');
+insert into trayecto (id_trains, id_stations) values (38, 'verde_olivo');
+insert into trayecto (id_trains, id_stations) values (10, 'rojo');
+insert into trayecto(id_trains,id_stations)values(12,'verde_y_gris');
+insert into trayecto (id_trains, id_stations) values (26, 'rojo');
+insert into trayecto (id_trains, id_stations) values (27, 'oro');
+insert into trayecto (id_trains, id_stations) values (28, 'morado');
+insert into trayecto (id_trains, id_stations) values (29, 'rojo');
+insert into trayecto (id_trains, id_stations) values (30, 'oro');
+insert into trayecto (id_trains, id_stations) values (31, 'rosa');
+insert into trayecto (id_trains, id_stations) values (32, 'naranja');
+insert into trayecto (id_trains, id_stations) values (33, 'rosa');
+insert into trayecto (id_trains, id_stations) values (34, 'rojo');
+insert into trayecto (id_trains, id_stations) values (35, 'rosa');
+insert into trayecto (id_trains, id_stations) values (36, 'morado');
+insert into trayecto (id_trains, id_stations) values (37, 'cian');
+insert into trayecto (id_trains, id_stations) values (38, 'morado');
+insert into trayecto(id_trains,id_stations)values(39,'café')
+insert into trayecto(id_trains,id_stations)values(40,'oro')
+insert into trayecto(id_trains,id_stations)values(41,'cian')
+insert into trayecto(id_trains,id_stations)values(42,'rosa')
+insert into trayecto(id_trains,id_stations)values(43,'verde olivo')
+insert into trayecto(id_trains,id_stations)values(44,'verde y gris')
+insert into trayecto(id_trains,id_stations)values(45,'morado')
+insert into trayecto(id_trains,id_stations)values(46,'café')
+insert into trayecto(id_trains,id_stations)values(47,'amarillo')
+insert into trayecto(id_trains,id_stations)values(48,'verde olivo')
+insert into trayecto(id_trains,id_stations)values(49,'rojo')
+insert into trayecto(id_trains,id_stations)values(1,'verde y gris')
